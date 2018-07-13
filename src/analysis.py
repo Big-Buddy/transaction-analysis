5e0723665ea11d853f9db21c282b031810519677
Alexandre Pelletier <pel_a@orwell.encs.concordia.ca>
2018-07-13 14:07:17 -0400

    debugged def probability

--- src/analysis.py
+++ src/analysis.py
@@ -3,14 +3,14 @@ from pyspark import SparkContext, SparkConf
 
 def probability_distribution(sample):
 	total_entries = sample.count()
-	reduced_sample = sample.map(lambda x: x[1], 1).reduceByKey(lambda a,b: a+b).collect()
+	reduced_sample = sample.map(lambda x: (x[1], 1)).reduceByKey(lambda a,b: a+b).collect()
 	return total_entries, reduced_sample
 
 conf = SparkConf().setAppName("seedbox-test").setMaster("local")
 sc = SparkContext(conf=conf)
 
-sampleRDD = sc.textFile("./data/testSamples.csv").mapPartitions(lambda x: csv.reader(x))
-transactionRDD = sc.textFile("./data/transData.csv").mapPartitions(lambda x: csv.reader(x))
+sampleRDD = sc.textFile("../data/testSamples.csv").mapPartitions(lambda x: csv.reader(x))
+transactionRDD = sc.textFile("../data/transData.csv").mapPartitions(lambda x: csv.reader(x))
 
 sampleHeader = sampleRDD.first()
 transactionHeader = transactionRDD.first()
